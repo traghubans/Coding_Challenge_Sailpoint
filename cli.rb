d8c9e9b474cdf03f5d2ed8ec45081beef4086d81
traghubans <traghubans@gmail.com>
2021-10-02 00:24:38 -0400

    Updated clifile to grab arguments

---
+++ cli.rb
@@ -0,0 +1,23 @@
+# Description: This file is handling CLI-parsing, Email Configuration, and Triggering
+
+# Project Imports 
+require 'rubygems'
+require 'httparty'
+
+# Declared Variables 
+args = Array.new
+email = ''
+
+# Test to see the arguments
+puts(ARGV.inspect)
+
+# Push arguments into args
+args.push(ARGV.inspect)
+
+# Error Handling
+
+# Parse Array for Email
+
+# Call API
+
+
